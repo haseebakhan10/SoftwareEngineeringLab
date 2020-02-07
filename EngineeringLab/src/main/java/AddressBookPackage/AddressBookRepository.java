1c3f31cf1dd07cbe3124d6a21a37cf49ad3d0bb8
Haseeb Khan <haseebakhan@LABS.SCE.CARLETON.CA>
2020-02-07 14:27:51 -0500

    TEST

--- EngineeringLab/src/main/java/AddressBookPackage/AddressBookRepository.java
+++
@@ -1,9 +0,0 @@
-package AddressBookPackage;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource(collectionResourceRel = "AddressBook", path = "addressbook")
-public interface AddressBookRepository extends CrudRepository<AddressBook, Integer> {
-    AddressBook findById(int id);
-}
